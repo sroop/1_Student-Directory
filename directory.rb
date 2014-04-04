d9cc80f09a6532eef8857207babb7b2b683ba6a7
Sroop <sroopsunar@hotmail.com>
2014-04-04 16:16:45 +0100

    refactoring - using the each method to iterate through each student in the array

--- directory.rb
+++ directory.rb
@@ -23,26 +23,10 @@ students = [
 
 puts "The students of my cohort at Makers Academy"
 puts "----------------------"
-# and then printing the list of students below by referencing their index number
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
-puts students[11]
-puts students[12]
-puts students[13]
-puts students[14] 
-puts students[15]
-puts students[16]
-puts students[17]
-puts students[18]
+# and then printing the list of students below by using the each method to iterate over each student in the array
+students.each do |student|
+	puts student
+end
 
-# second, I'm printing the total count 
+# printing the total count using the length method on the array
 puts "Overall, we have #{students.length} great students"
