bcfc7a87438208eaf203193cfc654c4d48d755c0
Sroop <sroopsunar@hotmail.com>
2014-04-04 16:13:31 +0100

    put students into an array and referencing each student by their index number. also using length method to calculate the total count

--- directory.rb
+++ directory.rb
@@ -1,26 +1,48 @@
-student_count = 19
+# adding the students into an array
+students = [
+"Steve Musgrave",
+"Sroop Sunar",
+"Colin Marshall",
+"Josh Fail-Brown",
+"Louise Lai",
+"Robin Doble",
+"Alex Wong",
+"Scott Dimmock",
+"Muhanad Al-Rubaiee",
+"Shelley Hope",
+"Will Hall",
+"Oliver Delevingne",
+"Nico",
+"Apostolis",
+"Stefania",
+"Robert Leon",
+"Emma Williams",
+"Joey Wolf",
+"Julie Walker"
+]
+
 puts "The students of my cohort at Makers Academy"
 puts "----------------------"
-# first I'm printing the list of students below
-puts "Steve Musgrave"
-puts "Sroop Sunar"
-puts "Colin Marshall"
-puts "Josh Fail-Brown"
-puts "Louise Lai"
-puts "Robin Doble"
-puts "Alex Wong"
-puts "Scott Dimmock"
-puts "Muhanad Al-Rubaiee"
-puts "Shelley Hope"
-puts "Will Hall"
-puts "Oliver Delevingne"
-puts "Nico"
-puts "Apostolis"
-puts "Stefania"
-puts "Robert Leon"
-puts "Emma Williams"
-puts "Joey Wolf"
-puts "Julie Walker"
+# and then printing the list of students below by referencing their index number
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
+puts students[9]
+puts students[10]
+puts students[11]
+puts students[12]
+puts students[13]
+puts students[14] 
+puts students[15]
+puts students[16]
+puts students[17]
+puts students[18]
 
 # second, I'm printing the total count 
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.length} great students"
