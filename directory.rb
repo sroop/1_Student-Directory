38284674216f6879e28738b45f5df29ff23d3fd4
Sroop <sroopsunar@hotmail.com>
2014-04-04 16:35:00 +0100

    refactoring - stored student data inside of hashes and amended the printing method

--- directory.rb
+++ directory.rb
@@ -1,24 +1,24 @@
 # adding the students into an array
 students = [
-"Steve Musgrave",
-"Sroop Sunar",
-"Colin Marshall",
-"Josh Fail-Brown",
-"Louise Lai",
-"Robin Doble",
-"Alex Wong",
-"Scott Dimmock",
-"Muhanad Al-Rubaiee",
-"Shelley Hope",
-"Will Hall",
-"Oliver Delevingne",
-"Nico",
-"Apostolis",
-"Stefania",
-"Robert Leon",
-"Emma Williams",
-"Joey Wolf",
-"Julie Walker"
+{ name: "Steve Musgrave", cohort: :march},
+{ name: "Sroop Sunar", cohort: :march},
+{ name: "Colin Marshall", cohort: :march},
+{ name: "Josh Fail-Brown", cohort: :march},
+{ name: "Louise Lai", cohort: :march},
+{ name: "Robin Doble", cohort: :march},
+{ name: "Alex Wong", cohort: :march},
+{ name: "Scott Dimmock", cohort: :march},
+{ name: "Muhanad Al-Rubaiee", cohort: :march},
+{ name: "Shelley Hope", cohort: :march},
+{ name: "Will Hall", cohort: :march},
+{ name: "Oliver Delevingne", cohort: :march},
+{ name: "Nico", cohort: :march},
+{ name: "Apostolis", cohort: :march},
+{ name: "Stefania", cohort: :march},
+{ name: "Robert Leon", cohort: :march},
+{ name: "Emma Williams", cohort: :march},
+{ name: "Joey Wolf", cohort: :march},
+{ name: "Julie Walker", cohort: :march},
 ]
 # creating three methods below
 def print_header
@@ -26,9 +26,9 @@ def print_header
 	puts "----------------------"
 end
 
-def print(names)
-	names.each do |name|
-		puts name
+def print(students)
+	students.each do |student|
+		puts "#{student[:name]} (#{student[:cohort]} cohort)"
 	end
 end
 
