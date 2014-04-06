93010d8b113422e526ba878421b15b389d4d5ede
Sroop <sroopsunar@hotmail.com>
2014-04-06 04:04:44 +0100

    using the case method to create more options for the user. add case completed.

--- directory.rb
+++ directory.rb
@@ -1,34 +1,86 @@
 # creating methods below
-def input_students
-	print "Instructions:\nPlease enter the student names and cohort.\nThen to finish, just hit return twice!\n\n"
-	# creating an empty students array which will override the array above
+def add_student
 	students = []
-	#get the first name to begin the process
 	puts "Enter the first name:"
 	first_name = gets.chomp
-	#while the name is not empty, repeat this code
-	while !first_name.empty? do
-		puts "Enter the last name:"
-		last_name = gets.chomp
-		until !last_name.empty? do
-			puts "You need to enter a last name!"
+		while !first_name.empty? do
+			puts "Enter the last name:"
 			last_name = gets.chomp
-		end
-		puts "Enter the cohort:"
-		cohort = gets.chomp
-		until !cohort.empty? do
-			puts "You need to enter a cohort!"
+				until !last_name.empty? do
+					puts "You need to enter a last name!"
+					last_name = gets.chomp
+				end
+			puts "Enter the cohort:"
 			cohort = gets.chomp
+				until !cohort.empty? do
+					puts "You need to enter a cohort!"
+					cohort = gets.chomp
+				end
+			students << { first_name: first_name, last_name: last_name, cohort: cohort }
+			puts "Now we have #{students.length} students"
+			# get another name from the user
+			puts "Next student! Enter the first name:"
+			first_name = gets.chomp
 		end
-		# add the student hash to the array
-		students << { first_name: first_name, last_name: last_name, cohort: cohort }
-		puts "Now we have #{students.length} students"
-		# get another name from the user
-		puts "Next student! Enter the first name:"
-		first_name = gets.chomp
-	end
-	#  return the array of students
 	students
+end
+
+def input_students
+	# asking the user to input an answer
+	puts "What would you like to do in the directory?"
+	puts "-Type 'add' to add a student"
+	puts "-Type 'edit' to edit a student"
+	puts "-Type 'delete' to delete a student"
+	puts "-Type 'list' to display the full list of students"
+	answer = gets.chomp
+	#using the case method to offer options for each answer
+	case answer
+	when "add"
+		add_student
+	when "edit"
+		puts "Enter the name and cohort of the student you'd like to edit."
+		puts "Enter the first name:"
+		first_name = gets.chomp
+		while !first_name.empty? do
+			puts "Enter the last name:"
+			last_name = gets.chomp
+			until !last_name.empty? do
+				puts "You need to enter a last name!"
+				last_name = gets.chomp
+			end
+			puts "Enter the cohort:"
+			cohort = gets.chomp
+			until !cohort.empty? do
+				puts "You need to enter a cohort!"
+				cohort = gets.chomp
+			end
+		end
+		if first_name == students[:first_name] && last_name == students[:last_name] && cohort == students[:cohort]
+			puts "Student found. Please re-enter the information."
+			puts "Enter the first name:"
+			first_name = gets.chomp
+			while !first_name.empty? do
+				puts "Enter the last name:"
+				last_name = gets.chomp
+				until !last_name.empty? do
+					puts "You need to enter a last name!"
+					last_name = gets.chomp
+				end
+			puts "Enter the cohort:"
+			cohort = gets.chomp
+				until !cohort.empty? do
+					puts "You need to enter a cohort!"
+					cohort = gets.chomp
+				end
+			end
+
+		else
+			puts "Student not found!"
+		end
+
+	when "delete"
+	when "list"
+	end
 end
 
 def print_header
